af18c6f9b8df25765c778e21a60e15d2520b2118
John Gosset <john.gosset@gmail.com>
2015-02-06 14:52:53 -0500

    Add script to display version info

---
+++ versions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# NB: the 2>&1 in command substitutions is for capturing stderr where necessary
+declare -A version
+version[awk]="$(awk --version | head -1)"
+version[bash]="$(bash --version | head -1)"
+version[dart]="$(dart --version 2>&1)"
+version[fish]="$(fish --version 2>&1)"
+version[node]="$(node --version)"
+version[lua]="$(lua -v)"
+version[php]="$(php --version | head -1)"
+version[python]="$(python3 --version)"
+version[ruby]="$(ruby --version)"
+version[tclsh]="$(tclsh <<< 'puts $tcl_version')"
+
+for lang in ${!version[*]}; do
+	printf "%8s: %s\n" $lang "${version[$lang]}"
+done | sort
