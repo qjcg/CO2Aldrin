12d2c06d1630eac4bd00f6a62942c340884aea8a
John Gosset <john.gosset@gmail.com>
2015-02-07 14:34:37 -0500

    Add .zsh

---
+++ bin/fizzbuzz.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+for i in {1..100}; do
+        if ! (( $i % 15 )); then
+                echo "fizzbuzz"
+        elif ! (( $i % 3 )); then
+                echo "fizz"
+        elif ! (( $i % 5 )); then
+                echo "buzz"
+        else
+                echo $i
+        fi
+done
