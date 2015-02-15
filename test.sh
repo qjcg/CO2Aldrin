6fbd48fef50e3b3a30e9a48f8e5350d816b94e70
John Gosset <john.gosset@gmail.com>
2015-02-15 10:32:55 -0800

    Use process substitution in test.sh instead of tempfiles

--- test.sh
+++ test.sh
@@ -2,16 +2,12 @@
 
 expected=expected.txt
 bindir=./bin
-tempdir=$(mktemp -d)
 
 # later checked via arithmetic evaluation where 0 == false
 all_passing=1
 
 for b in $bindir/*; do
-	log=$tempdir/$(basename $b).log
-	$b > $log
-
-	if diff $expected $log &>/dev/null; then
+	if diff $expected <($b) &>/dev/null; then
 		echo "OK:  $b"
 	else
 		echo "ERROR: $b"
@@ -19,6 +15,4 @@ for b in $bindir/*; do
 	fi
 done
 
-# remove output dir by default
-[[ $1 == '-k' ]] || rm -rf $tempdir
 (( $all_passing )) || exit 1
