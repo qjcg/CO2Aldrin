79109d6b4b738b2dd083f3ae79992c7491e6d60a
John Gosset <john.gosset@gmail.com>
2016-04-01 16:34:45 -0400

    Update test_fizzbuzz.sh

--- app/test/test_fizzbuzz.sh
+++ app/test/test_fizzbuzz.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
+# Compare all fizzbuzz command output to expected output
 
-expected=/app/expected.txt
-bindir=/app/bin
+expected=expected.log
+bindir=../bin
 
 # later checked via arithmetic evaluation where 0 == false
-all_passing=1
+all_passed=1
 
 for b in $bindir/fizzbuzz*; do
 	if diff $expected <($b) &>/dev/null; then
 		echo "PASSED: $b"
 	else
 		echo "FAILED: $b"
-		all_passing=0
+		all_passed=0
 	fi
 done
 
-(( $all_passing )) || exit 1
+(( $all_passed )) || exit 1
